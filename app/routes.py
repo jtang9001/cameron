9e0e8d19d8fc51781a78a3321757f2bb6fe5e67a
Jiayi Tang <jiayitang10@gmail.com>
2019-10-09 22:16:28 -0600

    last commit before switch to django?

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,19 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.places import places
 from app.forms import PlaceForm
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    form = PlaceForm()
+    if form.validate_on_submit():
+        print("Check in submitted for name {} at {}".format(
+            form.name.data, form.place.data
+        ))
+        return redirect("/")
     return render_template(
         "index.html", 
         places = places, 
-        form = PlaceForm()
+        form = form
     )
