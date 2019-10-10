ac8bb59b097feab43d4d3e26d59a412493b06881
Jiayi Tang <jiayitang10@gmail.com>
2019-10-09 21:28:06 -0600

    started template

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,17 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    places = [
+        {"name": "Cameron", "people": ["Jiayi"]},
+        {"name": "CCIS", "people": []},
+        {"name": "DICE", "people": ["Ameer", "Adam"]},
+        {"name": "ECERF", "people": []},
+        {"name": "ECHA", "people": []},
+        {"name": "Education", "people": []},
+        {"name": "NREF", "people": []}
+    ]
+    return render_template("index.html", places = places)
